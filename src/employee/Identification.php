65934102dd448af7023d5e16b8013968164d2b85
Kevin Loquencio <kevin_loquencio@cody.inc>
2021-09-10 14:58:18 +0800

    first commit

---
+++ src/employee/Identification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mgntlib\Employee;
+
+use App\Models\Employee as EmployeeModel;
+
+class Identification 
+{
+    const INITIAL_ID = 1000;
+    private $employee = null;
+    public function __construct(EmployeeModel $employee) {
+        $this->employee = $employee;
+    }
+
+    public function generateIdentification() {
+        $this->employee->employee_id = $this->makeId();
+        $this->employee->save();
+    }
+
+    private function makeId() {
+        $latestEmployee = EmployeeModel::select(['id'])->orderBy('id','desc')->first();
+        return static::INITIAL_ID + $latestEmployee->id++;
+    }
+}
